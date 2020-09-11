a1223e6b1d617060c06c2756455166df996b12cd
Vitor Araujo <vitoraraujo.dev@gmail.com>
2020-09-11 00:01:47 -0300

    Sistema de Alteração de Areas Especificas e suas Caracteristicas

--- backend/src/app/controllers/SpecificInfo/SpecificAreaController.js
+++ backend/src/app/controllers/SpecificInfo/SpecificAreaController.js
@@ -24,19 +24,99 @@ class SpecificAreaController {
     }
 
     const specificArea = await SpecificArea.findByPk(req.body.id);
+    const defaultCharacteristics = await Characteristic.findAll();
+    const { characteristics } = req.body;
 
+    const invalid = characteristics.filter(
+      (characteristic) =>
+        !defaultCharacteristics.find(
+          (defaultCharacteristic) => defaultCharacteristic.id === characteristic
+        )
+    );
+
+    if (invalid.length >= 1) {
+      return res
+        .status(400)
+        .json({ error: 'Essa característica não está registrada.' });
+    }
+
+    // If Area already exists, update it and it's characteristics
     if (specificArea) {
-      const { id, kind, area } = await specificArea.update({
-        ...req.body,
+      const areaCharacteristics = await AreaCharacteristic.findAll({
+        where: { area_id: specificArea.id },
+        attributes: ['id', 'area_id', 'characteristic_id'],
       });
+
+      await specificArea
+        .update({
+          ...req.body,
+        })
+        .then(async (result) => {
+          // Creates all new Characteristics
+          if (characteristics && characteristics.length > 0) {
+            const newCharacteristics = characteristics.filter(
+              (characteristic) =>
+                !areaCharacteristics.find(
+                  (areaCharacteristic) =>
+                    areaCharacteristic.characteristic_id === characteristic
+                )
+            );
+            if (newCharacteristics.length > 0) {
+              await AreaCharacteristic.bulkCreate(
+                newCharacteristics.map((characteristic) => ({
+                  characteristic_id: characteristic,
+                  area_id: specificArea.id,
+                }))
+              );
+            }
+          }
+          return result;
+        })
+        .then(async (result) => {
+          // Deletes not selected Characteristics
+          if (characteristics && areaCharacteristics.length > 0) {
+            const deleteCharacteristics = areaCharacteristics.filter(
+              (areaCharacteristic) =>
+                !characteristics.find(
+                  (characteristic) =>
+                    characteristic === areaCharacteristic.characteristic_id
+                )
+            );
+            if (deleteCharacteristics.length > 0)
+              await AreaCharacteristic.destroy({
+                where: {
+                  id: deleteCharacteristics.map(
+                    (characteristic) => characteristic.id
+                  ),
+                },
+              });
+          }
+          return result;
+        })
+        .then(async (result) =>
+          SpecificArea.findByPk(result.id, {
+            attributes: ['id', 'area', 'kind'],
+            include: [
+              {
+                model: Characteristic,
+                as: 'characteristics',
+                attributes: ['id', 'title'],
+                through: {
+                  model: AreaCharacteristic,
+                },
+              },
+            ],
+          })
+        )
+        .then((result) => res.json(result));
     } else {
+      // Creates new Area
       await SpecificArea.create({
         ...req.body,
         company_id: req.companyId,
       })
         .then(async (result) => {
-          const { characteristics } = req.body;
-
+          // Creates all new Characteristics
           if (characteristics && characteristics.length > 0) {
             const newCharacteristics = characteristics.map(
               (characteristic) => ({
@@ -44,8 +124,8 @@ class SpecificAreaController {
                 area_id: result.id,
               })
             );
-
-            await AreaCharacteristic.bulkCreate(newCharacteristics);
+            if (newCharacteristics.length > 0)
+              await AreaCharacteristic.bulkCreate(newCharacteristics);
           }
           return result;
         })
