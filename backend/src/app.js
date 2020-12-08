51eed98eaf08f80fd8b6be0bc4d1c7992f3bed96
Vitor Araujo <vitoraraujo.dev@gmail.com>
2020-12-08 18:11:55 -0300

    Adiciona Cors ao backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -14,6 +15,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
